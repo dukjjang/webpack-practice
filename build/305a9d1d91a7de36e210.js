7e15668b741d42795507ddac6e4fa9fef53b968e
dukjjang <jhd7292@naver.com>
2022-07-26 14:17:57 +0900

    first commit

---
+++ build/305a9d1d91a7de36e210.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+require("./app.css");
+
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById("root"));
---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./app.css";
+
+const App = () => {
+  return (
+    <div>
+      App + Webpack + Babel
+      <h1>Main Component Start!</h1>
+      <h2>webpack watch test</h2>
+    </div>
+  );
+};
+
+export default App;
